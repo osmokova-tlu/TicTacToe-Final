20e6288dc5cdfd667aa92cc8985255930cc2b3c0
Ahmed ElShenawy <elshenawy.ahmed@gmail.com>
2024-11-20 11:58:44 +0200

    Initial commit

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public TMP_Text buttonText;
+
+    private GameController gameController;
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+    public void SetSpace()
+    {
+        buttonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+}
